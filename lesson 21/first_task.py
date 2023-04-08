2064fa7d34b387cacc21d4fd9d74118ed545d05a
Pavel Smirnov <smirnov.pavelbrd2@gmail.com>
2023-04-08 15:27:44 +0200

    lesson 21 add gitignore

---
+++ lesson 21/first_task.py
@@ -0,0 +1,25 @@
+import logging
+
+class CustomFile:
+    def __init__(self, filename, mode):
+        self.file = open(filename, mode)
+        self.counter = 0
+        self.logger = logging.getLogger('CustomFile')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.file.close()
+        if exc_type is not None:
+            self.logger.error(f'An error occurred: {exc_type}, {exc_value}, {traceback}')
+        else:
+            self.logger.info(f'The file {self.file.name} was closed successfully')
+        return False
+
+    def write(self, text):
+        self.counter += 1
+        self.file.write(text)
+
+    def read(self):
+        return self.file.read()
